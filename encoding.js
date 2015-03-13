42e1065e1e3860a0faff9eb13a9bec9a4e3ca8e8
Shaun Carlson <shaun.carlson@gmail.com>
2015-03-13 01:36:16 -0600

    Added NRZ encoding, ability to enter new bit sequences, and adjust speed

--- encoding.js
+++ encoding.js
@@ -3,79 +3,116 @@
     //Private Property 
     var ecd = {};
     var _canvas = canvas;
-    var priv = 'is private';
-    var _startX = _canvas.width;
-    var _startY = 60;
+    var _defaultBitWidth = 40;
+    var _defaultBitHeight= 40;
+    var _bitWidth = 40;
+    var _bitHeight= 40;
+    var _startX = 0;
+    var _startY = [60];
+    var _sequence = ['0', '1', '1', '0', '1', '0', '0'];
+    var _squareWaves = [];
+    var _startOverValue = 0;
+    var _restart = false;
+    var _started = false;
+    var _speed = 1;
 
-    ecd.publ = 'public, yo!'
-    var _coords = [
-      [20, 0],
-      [0, -20],
-      [20, 0],
-      [0, 20],
-      [20, 0],
-      [0, -20],
-      [20, 0],
-      [0, 20],
-      [20, 0],
-      [0, -20],
-      [20, 0],
-      [0, 20],
-      [20, 0],
-      [0, -20],
-      [20, 0],
-      [0, 20],
-      [20, 0],
-      [0, -20],
-      [20, 0],
-      [0, 20],
-      [20, 0],
-      [0, -20],
-      [20, 0],
-      [0, 20],
-    ];
     ecd.draw = function() {
-      console.log(_canvas.width);
-      console.log(_canvas.height);
-      var c = _canvas.getContext('2d');
-      _draw();
-      // var x = 0,
-      //     y = 60;
-      // c.moveTo(x, y);
-      // for (var i = 0; i < _coords.length; i++) {
-      //   x = x + _coords[i][0];
-      //   y = y + _coords[i][1];
-      //   c.lineTo(x, y);
-      // }
-      // c.stroke();
-      // privFunc();
+      if (_started) {
+        _restart = true;
+      }
+
+      _start();
     };
 
+    ecd.setSequence = function(sequence) {
+      var s = sequence.split('');
+      for (var i = 0; i < s.length; i++) {
+        if (s[i] != '0' && s[i] != '1') return false;
+      }
+
+      _sequence = s;
+      _squareWaves = [];
+      _squareWaves.push(_buildNRZ());
+      _startOverValue = _canvas.width + (_sequence.length + 3) * _bitWidth;
+      this.draw();
+    }
+
+    ecd.setSpeed = function(speed) {
+      _speed = speed;
+    }
+
+    function _buildNRZ() {
+      var nrzCoords = [[_canvas.width, 0]];
+      var prev = null;
+      for (var i = 0; i < _sequence.length; i++) {
+        var cur = _sequence[i];
+        if (prev == null) {
+          if (cur == '0') {
+            nrzCoords.push([0, 1 * (_bitHeight / 2)]);
+          } else {
+            nrzCoords.push([0, -1 * (_bitHeight / 2)]);
+          }
+        } else if (prev != cur) {
+          if (prev == '0') {
+            nrzCoords.push([0, -1 * _bitHeight]);
+          } else {
+            nrzCoords.push([0, 1 * _bitHeight]);
+          }
+        }
+
+        nrzCoords.push([_bitWidth, 0]);
+        prev = cur;
+      }
+
+      if (prev == '0') {
+        nrzCoords.push([0, -1 * (_bitHeight / 2)]);
+      } else {
+        nrzCoords.push([0, 1 * (_bitHeight / 2)]);
+      }
+
+      return nrzCoords
+    }
+
     function _draw() {
-      console.log('draw!');
+      if (_restart) {
+        _startX = 0;
+        _restart = false;
+        return;
+      }
+
       var c = _canvas.getContext('2d');
-      // _canvas.width = _canvas.width;
-      c.clearRect(0, 0, canvas.width, canvas.height);
-      var x = _startX;
-      var y = _startY;
-      _startX -= 5;
+      c.clearRect(0, 0, _canvas.width, _canvas.height);
       c.beginPath();
-      c.moveTo(x, y);
-      for (var i = 0; i < _coords.length; i++) {
-        x += _coords[i][0];
-        y += _coords[i][1];
+      for (var j = 0; j < _startY.length; j++) {
+        var x = _startX;
+        var y = _startY[j];
+        c.moveTo(x, y);
+        for (var i = 0; i < _squareWaves[j].length; i++) {
+          x += _squareWaves[j][i][0];
+          y += _squareWaves[j][i][1];
+          c.lineTo(x, y);
+        }
+
+        x += 2 * _canvas.width;
         c.lineTo(x, y);
         c.stroke();
       }
 
-      if (_startX < -2 * _canvas.width) {
-        _startX = _canvas.width;
+      _startX -= _speed;
+      if (_startX < -1 * _startOverValue) {
+        _startX = 0;
       }
 
       setTimeout(function() { _draw(); }, 30);
     }
-    function privFunc() {
-      console.log('privFunc');
+
+    function _start() {
+      if (_restart) {
+        setTimeout(function() { _start(); }, 10);
+      } else {
+        _started = true;
+        _draw();
+      }
     }
 
     return ecd;
